5a22dff46cc5555fbe4f525dd71b6594c4b6ad66
Vlad <103254700+zVzLzAzDz@users.noreply.github.com>
2022-09-11 17:22:20 +0300

    Update Task01

--- src/Task01.java
+++ src/Task01.java
@@ -1,5 +1,9 @@
 public class Task01 {
     public static void main(String[] args) {
         System.out.println("Hello GitHub!!!");
+        System.out.println("Hello GitHub!!!");
+        System.out.println("Hello GitHub!!!");
+        System.out.println("Hello GitHub!!!");
+        System.out.println("Hello GitHub!!!");
     }
 }
